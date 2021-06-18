7867549568c27c69986aaa197e20fef3fb9bb90b
Federico Brigante <me@fregante.com>
2021-06-18 13:58:38 +0700

    Replace split for loops with entries-generating map

--- index.js
+++ index.js
@@ -7,17 +7,8 @@ const pProps = (input, mapper = (value => value), options) => {
 	const entries = isMap ? [...map.entries()] : Object.entries(map);
 	const awaitedEntries = entries.map(async ([key, value]) => [key, await value]);
 	const values = await pMap(awaitedEntries, ([key, value]) => mapper(value, key), options);
-	const result = isMap ? new Map() : {};
-
-	for (const [index, key] of entries) {
-		if (isMap) {
-			result.set(key, values[index]);
-		} else {
-			result[key] = values[index];
-		}
-	}
-
-	return result;
+	const mappedEntries = values.map((value, index) => [entries[index], value]);
+	return isMap ? new Map(mappedEntries) : Object.fromEntries(mappedEntries);
 };
 
 module.exports = pProps;
