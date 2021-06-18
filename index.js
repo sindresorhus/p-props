059444c1e10c2a97c37e1b5aaecbbfd66990e313
Federico <me@fregante.com>
2021-06-18 15:02:16 +0700

    Passing tests; engines.node = v12

--- index.js
+++ index.js
@@ -1,16 +1,12 @@
 'use strict';
 const pMap = require('p-map');
 
-const pProps = async (input, mapper, options) => {
+const pProps = async (input, mapper = (value => value), options = undefined) => {
 	const isMap = input instanceof Map;
-	const entries = isMap ? [...map.entries()] : Object.entries(map);
-	const awaitedEntries = entries.map(async ([key, value]) => [key, await value]);
-	if (!mapper && !options) {
-		return isMap ? new Map(awaitedEntries) : Object.fromEntries(awaitedEntries);
-	}
-
-	const values = await pMap(awaitedEntries, ([key, value]) => mapper(value, key), options);
-	const mappedEntries = awaitedEntries.map(([key], index) => [key, value[index]]);
+	const entries = isMap ? [...input.entries()] : Object.entries(input);
+	const promisedEntries = entries.map(async ([key, value]) => [key, await value]);
+	const values = await pMap(promisedEntries, ([key, value]) => mapper(value, key), options);
+	const mappedEntries = entries.map(([key], index) => [key, values[index]]);
 	return isMap ? new Map(mappedEntries) : Object.fromEntries(mappedEntries);
 };
 
