ee81be61c0821fd4c5599c84cceb51465a950463
Federico Brigante <me@fregante.com>
2021-06-18 13:59:55 +0700

    Skip p-map altogether if unused

--- index.js
+++ index.js
@@ -1,11 +1,14 @@
 'use strict';
 const pMap = require('p-map');
 
-// eslint-disable-next-line default-param-last
-const pProps = (input, mapper = (value => value), options) => {
+const pProps = (input, mapper, options) => {
 	const isMap = input instanceof Map;
 	const entries = isMap ? [...map.entries()] : Object.entries(map);
 	const awaitedEntries = entries.map(async ([key, value]) => [key, await value]);
+	if (!mapper && !options) {
+		return isMap ? new Map(awaitedEntries) : Object.fromEntries(awaitedEntries)
+	}
+
 	const values = await pMap(awaitedEntries, ([key, value]) => mapper(value, key), options);
 	const mappedEntries = values.map((value, index) => [entries[index], value]);
 	return isMap ? new Map(mappedEntries) : Object.fromEntries(mappedEntries);
