f01bd641cb347e7a0784420aa0531eceb2b19584
Federico Brigante <me@fregante.com>
2021-06-18 13:49:47 +0700

    Merge map and object handling

--- index.js
+++ index.js
@@ -1,35 +1,23 @@
 'use strict';
 const pMap = require('p-map');
 
-const map = async (map, mapper, options) => {
-	const awaitedEntries = [...map.entries()].map(async ([key, value]) => [key, await value]);
-	const values = await pMap(awaitedEntries, ([key, value]) => mapper(value, key), options);
-	const result = new Map();
-
-	for (const [index, key] of [...map.keys()].entries()) {
-		result.set(key, values[index]);
-	}
-
-	return result;
-};
-
-const object = async (map, mapper, options) => {
-	const awaitedEntries = Object.entries(map).map(async ([key, value]) => [key, await value]);
-	const values = await pMap(awaitedEntries, ([key, value]) => mapper(value, key), options);
-	const result = {};
-
-	for (const [index, key] of Object.keys(map).entries()) {
-		result[key] = values[index];
-	}
-
-	return result;
-};
-
 // eslint-disable-next-line default-param-last
 const pProps = (input, mapper = (value => value), options) => {
-	return input instanceof Map ?
-		map(input, mapper, options) :
-		object(input, mapper, options);
+	const isMap = input instanceof Map;
+	const entries = isMap ? [...map.entries()] : Object.entries(map);
+	const awaitedEntries = entries.map(async ([key, value]) => [key, await value]);
+	const values = await pMap(awaitedEntries, ([key, value]) => mapper(value, key), options);
+	const result = isMap ? new Map() : {};
+
+	for (const [index, key] of entries) {
+		if (isMap) {
+			result.set(key, values[index]);
+		} else {
+			result[key] = values[index];
+		}
+	}
+
+	return result;
 };
 
 module.exports = pProps;
