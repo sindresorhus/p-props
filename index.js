5331e8064c681cd3d0bbeec5a113f08c27b6d0b7
Sindre Sorhus <sindresorhus@gmail.com>
2021-06-18 15:34:52 +0700

    Update index.js
    Co-authored-by: Federico Brigante <me@fregante.com>

--- index.js
+++ index.js
@@ -4,8 +4,7 @@ const pMap = require('p-map');
 const pProps = async (input, mapper = (value => value), options = undefined) => {
 	const isMap = input instanceof Map;
 	const entries = isMap ? [...input.entries()] : Object.entries(input);
-	const promisedEntries = entries.map(async ([key, value]) => [key, await value]);
-	const values = await pMap(promisedEntries, ([key, value]) => mapper(value, key), options);
+	const values = await pMap(entries, async ([key, value]) => mapper(await value, key), options);
 	const mappedEntries = entries.map(([key], index) => [key, values[index]]);
 	return isMap ? new Map(mappedEntries) : Object.fromEntries(mappedEntries);
 };
